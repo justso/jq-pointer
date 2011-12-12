90c43bccff411c1aa5b91689fde4a9bd8b32c06c
dvdrtrgn <dvdrtrgn@gmail.com>
2011-12-12 14:20:43 -0600

    first commit

---
+++ pointer.js
@@ -0,0 +1,27 @@
+// Show the depth stack @ fixed position in upper right
+// List the pivotal choices for debugging
+
+var Pointer = {
+    divs : [],
+    init : function(){
+        var me = Pointer;
+        $.loadCssFor('pointer');
+        me.div = $('<div id="Pointer">').appendTo('body');
+    },
+    /**
+     * Add placard to floating list
+     * @param idx {number} position in list
+     * @param str {string} milestone text
+     */
+    add : function(idx, str){
+        var newDiv = $('<div class="Pointer">').text(str)
+        ,   num = this.divs.length
+        ,   i = num;
+        if (idx >= num) // not created yet
+            for (; i <= idx; i++) // make enough up to including idx
+                this.divs[i] = $('<div>').appendTo(this.div);
+        this.divs[idx].replaceWith(newDiv);
+        this.divs[idx] = newDiv;
+    }
+}
+$(Pointer.init);
